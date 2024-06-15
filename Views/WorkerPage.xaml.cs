9f1265d965df79b8f6407a736d534c40c7c8b486
Leonid <98675428+straponessi@users.noreply.github.com>
2024-06-15 07:01:25 +0300

    Update WorkerPage.xaml.cs

--- Views/WorkerPage.xaml.cs
+++ Views/WorkerPage.xaml.cs
@@ -26,3 +26,67 @@ namespace DEMO_WPF.NET8.Views
         }
     }
 }
+        private string _searchQuery;
+        public string SearchQuery
+        {
+            get { return _searchQuery; }
+            set
+            {
+                if (_searchQuery != value)
+                {
+                    _searchQuery = value;
+                    OnPropertyChanged(nameof(SearchQuery));
+                    RefreshDrivers();
+                }
+            }
+        }
+
+        public ICommand AddCommand { get; set; }
+        public ICommand ChangeCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+
+        public DriversVM()
+        {
+            CheckAutoRiz();
+            db.Drivers.Load();
+            Drivers = db.Drivers.Local.ToObservableCollection();
+
+            AddCommand = new RelayCommand(AddDriver);
+            ChangeCommand = new RelayCommand(SaveChanges, CanExecute);
+            DeleteCommand = new RelayCommand(DeleteExecute, CanExecute);
+        }
+
+        private void AddDriver(object parametr)
+        {
+            Window window = new AddDriverWindow();
+            window.ShowDialog();
+        }
+
+        private void RefreshDrivers()
+        {
+            var query = GetBaseDriversQuery();
+
+            if (!string.IsNullOrEmpty(SearchQuery))
+            {
+                bool isNumber = int.TryParse(SearchQuery, out int searchId);
+
+                query = query.Where(o => (isNumber && o.Id == searchId) ||
+                                         o.Fullname.Contains(SearchQuery) ||
+                                         o.Contact.Contains(SearchQuery) ||
+                                         o.License.Contains(SearchQuery)); 
+            }
+            Drivers = new ObservableCollection<Driver>(query.ToList());
+        }
+
+        private IQueryable<Driver> GetBaseDriversQuery()
+        {
+            return from driver in db.Drivers
+                   select new Driver
+                   {
+                       Id = driver.Id,
+                       Fullname = driver.Fullname,
+                       Contact = driver.Contact,
+                       License = driver.License,
+                       Status = driver.Status,
+                   };
+        }
